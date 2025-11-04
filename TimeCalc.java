2e538b230da56556280c7f8a69b545c20c6c30c8
Liron Amar <liron.amar@post.runi.ac.il>
2025-11-04 11:33:25 +0200

    1 is done

--- TimeCalc.java
+++ TimeCalc.java
@@ -1,5 +1,20 @@
 public class TimeCalc {
     public static void main(String[] args) {
-        // Replace this comment with your code
+        
+        int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
+		int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
+        int minAdd = Integer.parseInt(args[1]); // 1440 minutes in a day
+
+        int sum = (hours * 60) + minutes + minAdd;
+
+        sum = sum % 1440;
+
+        System.out.println((sum / 60) / 10 + "" + (sum / 60) % 10 + ":" + (sum % 60) / 10 + "" + (sum % 60) % 10);
+        
+        
+        
+        
+        
+        
     }
 }
